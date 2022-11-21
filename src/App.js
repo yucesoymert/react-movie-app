c765ca4d71ef132645915abf8dff9fdb6676c16d
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-21 17:15:08 +0300

    update routing in app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "swiper/swiper.min.css";
 import "./App.scss";
 
+//pages
+import Home from "../src/pages/Home";
+import Catalog from "../src/pages/Catalog";
+import Detail from "../src/pages/Detail";
+
+//components
+import Header from "../src/components/header/Header";
+import Footer from "../src/components/footer/Footer";
+
 function App() {
-  return <div>Movie App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/:category/search/:keyword" element={<Catalog />} />
+        <Route path="/:category/:id" element={<Detail />} />
+        <Route path="/:category" element={<Catalog />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
