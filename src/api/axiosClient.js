8eec19c9ca32e0d8aac7998fc0d43760de724b7e
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-23 20:06:51 +0300

    update axios client

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import queryString from "query-string";
 
 import apiConfig from "./apiConfig";
 
@@ -8,23 +7,21 @@ const axiosClient = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  paramsSerializer: (params) =>
-    queryString.stringify({
-      ...params,
-      apiKey: apiConfig.apiKey,
-    }),
 });
 
-axiosClient.interceptors.request.use(async(config) = config);
+axiosClient.interceptors.request.use(async (config) => ({
+  ...config,
+  params: {
+    ...config.params,
+    api_key: apiConfig.apiKey,
+  },
+}));
 
-axiosClient.interceptors.response.use(async(response) => {
-    if(response && response.data) {
-        return response.data;
-    }
-    return response;
-}, (error) => { 
-    throw error;
+axiosClient.interceptors.response.use(async (response) => {
+  if (response && response.data) {
+    return response.data;
+  }
+  return response;
 });
 
 export default axiosClient;
-
