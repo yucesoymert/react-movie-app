0fff7224dfd4ef981673401206f8c5b14bbdd9df
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-23 20:07:57 +0300

    update footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import scss
+import './footer.scss'
+
+ // import png
+ import footerbg from '../../assets/footer-bg.jpg';
+ import logo from '../../assets/meflix.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer' style={{backgroundImage: `url(${footerbg})`}}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className='logo'>
+            <img src={logo} alt='' />
+            <Link to="/">Meflix</Link>
+          </div>
+        </div> 
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to=''>Home</Link>
+            <Link to=''>Contact me</Link>
+            <Link to=''>About me</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to=''>You must watch</Link>
+            <Link to=''>Recent release</Link>
+            <Link to=''>Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
