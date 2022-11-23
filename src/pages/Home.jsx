63f9c12f852713a762fe3caac7f9e90a86a8bf64
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-23 20:11:28 +0300

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { OutlineButton } from '../components/button/Button'
+
+// import api config
+import { category, movieType, tvType  } from '../api/tmdbApi';
+
+//components
+import HeroSlide from '../components/hero-slide/HeroSlide'
+import MovieList from '../components/movie-list/MovieList'
 
 const Home = () => {
   return (
-    <div>Home 🍿</div>
+    <>
+      <HeroSlide />
+      <div className="container">
+        {/* trending movies */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to='/movie'>
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.popular} />
+        </div>
+
+        {/* top rated movies */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated Movies</h2>
+            <Link to='/movie'>
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated} />
+        </div>
+
+        {/* trending tv*/}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV</h2>
+            <Link to='/tv'>
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular} />
+        </div>
+      </div>
+    </>
   )
 }
 
