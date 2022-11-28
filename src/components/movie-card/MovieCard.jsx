4bb07d73507a9a3110da91aba35e705efc4922a4
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-28 15:23:52 +0300

    add movie card component

---
+++ src/components/movie-card/MovieCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import scss
+import './movie-card.scss'
+
+// import api config
+import { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+
+// components
+import Button from '../button/Button'
+
+const MovieCard = props => {
+
+    const item = props.item
+
+    const link = '/' + category[props.category] + '/' + item.id;
+
+    const background = apiConfig.w500Image(item.backdrop_path || item.poster_path);
+
+  return (
+    <Link to={link}>
+        <div className='movie-card' style={{backgroundImage: `url(${background})`}}>
+            <Button>
+                <i className='bx bx-play bx-md'></i>
+            </Button>
+        </div>
+        <h3 className='movie-title'>{item.title || item.name}</h3>
+    </Link>
+    )
+}
+
+export default MovieCard
