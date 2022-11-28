f52f8a3816e58eb5765a3c6b4512d53471a4c851
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-28 15:31:08 +0300

    change web site name in header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () =>{
       <div className='header__wrap container' >
         <div className='logo'>
           <img src={logo} alt='' />
-          <Link to="/">Meflix</Link>
+          <Link to="/">Myflix</Link>
         </div>
         <ul className='header__nav'>
           {
