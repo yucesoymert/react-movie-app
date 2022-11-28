dde7906207ec6f461172957fed2fa50f3d459bd1
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-28 23:05:00 +0300

    update detail page src

--- src/App.js
+++ src/App.js
@@ -7,25 +7,28 @@ import "./App.scss";
 //pages
 import Home from "../src/pages/Home";
 import Catalog from "../src/pages/Catalog";
-import Detail from "../src/pages/Detail";
+import Detail from "../src/pages/detail/Detail";
 
 //components
 import Header from "../src/components/header/Header";
 import Footer from "../src/components/footer/Footer";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/:category/search/:keyword" element={<Catalog />} />
-        <Route path="/:category/:id" element={<Detail />} />
-        <Route path="/:category" element={<Catalog />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route
+                    path="/:category/search/:keyword"
+                    element={<Catalog />}
+                />
+                <Route path="/:category/:id" element={<Detail />} />
+                <Route path="/:category" element={<Catalog />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
