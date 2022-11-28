146b65125163fe7add7f6861f32bcc53c4fc2551
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-28 15:30:26 +0300

    update catalog component

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+// import components
+import PageHeader from '../components/page-header/PageHeader';
+import MovieGrid from '../components/movie-grid/MovieGrid';
+
+// import category api
+import { category as cate } from '../api/tmdbApi';
 
 const Catalog = () => {
+
+  const { category } = useParams();
+
   return (
-    <div>Catalog</div>
+    <>
+      <PageHeader>
+        {category === cate.movie ? 'Movies' : 'TV series'}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
+    </>
   )
 }
 
