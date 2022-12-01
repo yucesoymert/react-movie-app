bcc97864db45613a86823730287decb40c9f22a7
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-12-02 00:01:02 +0300

    update http to https

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,9 @@
 const apiConfig = {
-  baseUrl: "http://api.themoviedb.org/3/",
-  apiKey: "38923148a593372ceff8d77caf1aca40",
-  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+    baseUrl: "https://api.themoviedb.org/3/",
+    apiKey: "38923148a593372ceff8d77caf1aca40",
+    originalImage: (imgPath) =>
+        `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
 
 export default apiConfig;
